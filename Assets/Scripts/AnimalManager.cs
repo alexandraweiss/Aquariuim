e1412a25db86344c97e24522b6867939c7738c90
Alexandra Weiß <baronesse.poubelle@gmail.com>
2020-07-16 14:10:24 +0200

    Initial commit
    Empty project with DOTS and ECS packages installed.

---
+++ Assets/Scripts/AnimalManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Physics;
+
+public class AnimalManager : MonoBehaviour
+{
+    protected EntityManager entityManager;
+
+    private void Awake()
+    {
+        entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+
+    }
+}
