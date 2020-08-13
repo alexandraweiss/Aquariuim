00a808daa7892d277db52e6769d485480e8fe065
Alexandra Weiß <baronesse.poubelle@gmail.com>
2020-08-13 21:22:46 +0200

    Added class for static values

---
+++ Assets/Scripts/StaticValues.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticValues 
+{
+    public static readonly float MAX_Y = 500f;
+    public static readonly float MIN_Y = - 500f;
+
+    public static readonly float MAX_X = 1000f;
+    public static readonly float MIN_X = - 1000f;
+
+    public static readonly float MAX_Z = 1000f;
+    public static readonly float MIN_Z = - 1000f;
+}
