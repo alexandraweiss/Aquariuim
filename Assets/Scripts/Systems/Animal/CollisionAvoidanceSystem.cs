2c382ed242806592c1710b44f84bb9226e332b00
Alexandra Weiß <baronesse.poubelle@gmail.com>
2020-08-20 16:44:07 +0200

    #2 collision avoidance takes direction and speed of the other entity into account

--- Assets/Scripts/Systems/Animal/CollisionAvoidanceSystem.cs
+++ Assets/Scripts/Systems/Animal/CollisionAvoidanceSystem.cs
@@ -5,7 +5,8 @@ using Unity.Physics.Systems;
 using Unity.Burst;
 using Unity.Rendering;
 using Unity.Mathematics;
-
+using UnityEngine;
+using Unity.Transforms;
 
 /// <summary>
 /// System handling collision avoidance. 
@@ -15,12 +16,12 @@ public class CollisionAvoidanceSystem : SystemBase
     private BuildPhysicsWorld buildPhysicsWorld;
     private StepPhysicsWorld stepPhysicsWorld;
 
-
     [BurstCompile]
     struct NotificationJob : ITriggerEventsJob
     {
         public ComponentDataFromEntity<AnimalTag> animalTag;
         public ComponentDataFromEntity<AnimalMovementData> mvmtData;
+        public ComponentDataFromEntity<Translation> translation;
 
         public void Execute(TriggerEvent triggerEvent)
         {
@@ -28,9 +29,20 @@ public class CollisionAvoidanceSystem : SystemBase
             {
                 AnimalMovementData mvmtDataA = mvmtData[triggerEvent.Entities.EntityA];
                 AnimalMovementData mvmtDataB = mvmtData[triggerEvent.Entities.EntityB];
+                float3 positionA = translation[triggerEvent.Entities.EntityA].Value;
 
-                quaternion newRotation = quaternion.RotateY(0.262f);
-                float3 newDirection = math.rotate(newRotation, mvmtDataB.direction);
+                float speedA = mvmtDataA.movementSpeed;
+                float speedB = mvmtDataB.movementSpeed;
+                float maxSpeed = math.max(speedA, speedB);
+
+                float dotProduct = math.dot(math.normalize(mvmtDataA.targetDirection - positionA), math.normalize(mvmtDataB.targetDirection - positionA));
+
+                float angle = 0.262f;
+                angle += 1f - math.abs(dotProduct);
+                angle += (1f - (maxSpeed * 0.1f));
+                angle = math.clamp(angle, 0f, StaticValues.AVOIDANCE_MIN_ANGLE);
+                quaternion newRotation = quaternion.RotateY(angle);
+                float3 newDirection = math.rotate(newRotation, mvmtDataA.direction);
                 newDirection = math.normalizesafe(newDirection);
                 // Set EntityA's target direction
                 mvmtDataA.targetDirection = newDirection;
@@ -43,6 +55,7 @@ public class CollisionAvoidanceSystem : SystemBase
     {
         buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
         stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+        Enabled = true;
     }
 
     [BurstCompile]
@@ -52,6 +65,7 @@ public class CollisionAvoidanceSystem : SystemBase
         {
             animalTag = GetComponentDataFromEntity<AnimalTag>(),
             mvmtData = GetComponentDataFromEntity<AnimalMovementData>(),
+            translation = GetComponentDataFromEntity<Translation>(),
         };
 
         Dependency = notificationJob.Schedule(stepPhysicsWorld.Simulation, ref buildPhysicsWorld.PhysicsWorld, Dependency);
